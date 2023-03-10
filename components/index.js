1ec6281317d2f3ab5cf8ae581ad5f1a2422c27ef
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-03-10 15:20:18 +0300

    updated

--- components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CatagoryCard } from "../components/CatagoryCard";
-export { default as FeaturedRow } from "../components/FeaturedRow";
-export { default as RestaurantCards } from "../components/RestaurantCards";
